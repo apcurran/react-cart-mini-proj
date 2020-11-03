40a2501c3eece5efd2c6908a9a36263425d91a3e
Alex Curran <alexcurran.va@gmail.com>
2020-11-03 15:04:35 -0600

    Removed build dir from .gitignore file

---
+++ precache-manifest.26b3172861ce6289b624b2f69deac63f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74dbc9d7e37bd3d71944448de1b33488",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e28a5508e4440137426c",
+    "url": "./static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "./static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28a5508e4440137426c",
+    "url": "./static/js/main.b7a45bd2.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
